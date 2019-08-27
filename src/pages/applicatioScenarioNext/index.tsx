c1d77a983b0d73b27033ca9c6773c564fba245b7
cyqyoyoyo <949095688@qq.com>
2019-08-27 09:56:20 +0800

    commit

--- src/pages/applicatioScenarioNext/index.tsx
+++ src/pages/applicatioScenarioNext/index.tsx
@@ -25,15 +25,17 @@ class applicatioScenarioNext extends React.Component {
     if (!localStorage.getItem('solutionGroup')) {
       // @ts-ignore
       const { solutionGroup } = this.props;
-      localStorage.setItem('solutionGroup', JSON.stringify({ solutionGroup }))
+      localStorage.setItem('solutionGroup', JSON.stringify({ solutionGroup }));
     }
     const { solutionGroup } = JSON.parse(localStorage.getItem('solutionGroup') as string);
     // eslint-disable-next-line max-len
-    const buttonGroup = solutionGroup.filter((item: { name: any; }) => item.name === match.params.name)[0].solutionSonGroup;
+    const buttonGroup = solutionGroup.filter(
+      (item: { name: any }) => item.name === match.params.name,
+    )[0].solutionSonGroup;
     this.setState({
       pageName: match.params.name,
       buttonGroup,
-    })
+    });
   }
 
   // nextPage = () => {
@@ -45,38 +47,47 @@ class applicatioScenarioNext extends React.Component {
   //   }
   // }
   routerButtonClick = (url: any) => {
-    window.open(url, '', 'fullscreen = yes,channelmode = yes')
-  }
+    window.open(url);
+  };
 
   render() {
     const { buttonGroup, searchResult, startIndex, endIndex, pageName } = this.state;
     return (
-        <div style={{ width: '100%', height: '100%' }} className= "overview">
-          <div>
-            <h1 style={{ marginLeft: '1.5%', fontSize: '20px' }}>{pageName}</h1>
-          </div>
-          <div className={styles.resultWrap}>
-            {
-              // @ts-ignore
-              buttonGroup.map((item, index) => {
-                // eslint-disable-next-line react/jsx-no-bind,radix
-                const groupIndex = parseInt(String(index));
-                if (groupIndex >= startIndex && groupIndex < endIndex) {
-                  // @ts-ignore
-                  if (!searchResult || buttonGroup[index].name.indexOf(searchResult) !== -1) {
-                    // eslint-disable-next-line max-len
-                    // @ts-ignore
-                    // eslint-disable-next-line max-len,react/jsx-no-bind
-                    return <Cards content={buttonGroup[index].name} state={buttonGroup[index].state} opacityTime={parseInt(index)*100} collected={buttonGroup[index].collected} handleClick={this.routerButtonClick.bind(this, buttonGroup[index].url)}/>
-                  } return null
-                } return null
-              })
-            }
-          </div>
+      <div style={{ width: '100%', height: '100%' }} className="overview">
+        <div>
+          <h1 style={{ marginLeft: '1.5%', fontSize: '20px' }}>{pageName}</h1>
         </div>
+        <div className={styles.resultWrap}>
+          {// @ts-ignore
+          buttonGroup.map((item, index) => {
+            // eslint-disable-next-line react/jsx-no-bind,radix
+            const groupIndex = parseInt(String(index));
+            if (groupIndex >= startIndex && groupIndex < endIndex) {
+              // @ts-ignore
+              if (!searchResult || buttonGroup[index].name.indexOf(searchResult) !== -1) {
+                // eslint-disable-next-line max-len
+                // @ts-ignore
+                // eslint-disable-next-line max-len,react/jsx-no-bind
+                return (
+                  <Cards
+                    content={buttonGroup[index].name}
+                    state={buttonGroup[index].state}
+                    opacityTime={parseInt(index) * 100}
+                    collected={buttonGroup[index].collected}
+                    handleClick={this.routerButtonClick.bind(this, buttonGroup[index].url)}
+                  />
+                );
+              }
+              return null;
+            }
+            return null;
+          })}
+        </div>
+      </div>
     );
   }
 }
 export default connect(({ global }: ConnectState) => ({
-  solutionGroup: global.solutionGroup, solutionRouter: global.solutionRouter,
+  solutionGroup: global.solutionGroup,
+  solutionRouter: global.solutionRouter,
 }))(applicatioScenarioNext);
