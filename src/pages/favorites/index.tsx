61475df612a01db28ddb02af7df98ce23914790d
cyqyoyoyo <949095688@qq.com>
2019-09-05 14:48:35 +0800

    modify requirements  2019/9/5

--- src/pages/favorites/index.tsx
+++ src/pages/favorites/index.tsx
@@ -4,7 +4,7 @@ import { ConnectState } from '@/models/connect';
 import styles from './index.scss';
 import Cards from '@/components/Cards';
 import HeaderSearch from '@/components/HeaderSearch';
-import router from "umi/router";
+import router from 'umi/router';
 
 // @ts-ignore
 @connect(({ global }) => ({
@@ -22,20 +22,20 @@ class favorites extends React.Component {
   };
 
   componentDidMount(): void {
-    if(localStorage.getItem('solutionGroup')){
+    if (localStorage.getItem('solutionGroup')) {
       const { solutionGroup } = JSON.parse(localStorage.getItem('solutionGroup') as string);
       const favoritesArr: any[] | never[] = [];
       solutionGroup.forEach(item => {
         item.solutionSonGroup.forEach(items => {
           if (items.collected) {
-            favoritesArr.push(items)
+            favoritesArr.push(items);
           }
-        })
-      })
+        });
+      });
       this.setState({
         pageName: this.props.match.params.name,
         buttonGroup: favoritesArr,
-      })
+      });
     }
   }
 
@@ -48,51 +48,75 @@ class favorites extends React.Component {
   //   }
   // }
   searchHandle = (value: any) => {
-    console.log(value)
+    console.log(value);
     this.setState({
       searchResult: value,
-    })
-  }
+    });
+  };
 
-  routerButtonClick = (url: string | undefined) => {
+  routerButtonClick = (url, name) => {
     const { dispatch } = this.props;
     dispatch({
       type: 'global/setIframeUrl',
       payload: { iframeUrl: url },
-    })
-    router.push('/index/applicatioScenarioIndex')
-  }
+    });
+    router.push('/index/applicatioScenarioIndex/' + name);
+  };
 
   render() {
     const { buttonGroup, searchResult, startIndex, endIndex } = this.state;
     return (
       <div className="overview">
         <div style={{ overflow: 'hidden' }}>
-          <h1 style={{ marginLeft: '1.5%', float: 'left', fontSize: '20px', marginRight: '40px', color:'rgba(255, 255, 255, 0.65)' }}>收藏夹</h1>
-          <HeaderSearch searchHandle={this.searchHandle}/>
+          <h1
+            style={{
+              marginLeft: '1.5%',
+              float: 'left',
+              fontSize: '20px',
+              marginRight: '40px',
+              color: 'rgba(255, 255, 255, 0.65)',
+            }}
+          >
+            收藏夹
+          </h1>
+          <HeaderSearch searchHandle={this.searchHandle} />
         </div>
         <div className={styles.resultWrap}>
-          {
-            buttonGroup.map((item, index) => {
-              // eslint-disable-next-line react/jsx-no-bind
-              const groupIndex = parseInt(index);
-              // 可能分页
-              if (groupIndex >= startIndex && groupIndex < endIndex) {
-                // 搜索结果过滤
-                if (!searchResult || buttonGroup[index].name.indexOf(searchResult) !== -1) {
-                  // eslint-disable-next-line max-len
-                  // @ts-ignore
-                  // eslint-disable-next-line max-len
-                  return <Cards content={buttonGroup[index].name} ischildrenCard={true}  opacityTime={parseInt(index)*100} state={buttonGroup[index].state} handleClick={this.routerButtonClick.bind(this, buttonGroup[index].url)} src={buttonGroup[index].image}/>
-                } return null
-              } return null
-            })
-          }
+          {buttonGroup.map((item, index) => {
+            // eslint-disable-next-line react/jsx-no-bind
+            const groupIndex = parseInt(index);
+            // 可能分页
+            if (groupIndex >= startIndex && groupIndex < endIndex) {
+              // 搜索结果过滤
+              if (!searchResult || buttonGroup[index].name.indexOf(searchResult) !== -1) {
+                // eslint-disable-next-line max-len
+                // @ts-ignore
+                // eslint-disable-next-line max-len
+                return (
+                  <Cards
+                    content={buttonGroup[index].name}
+                    ischildrenCard={true}
+                    opacityTime={parseInt(index) * 100}
+                    state={buttonGroup[index].state}
+                    handleClick={this.routerButtonClick.bind(
+                      this,
+                      buttonGroup[index].url,
+                      buttonGroup[index].name,
+                    )}
+                    src={buttonGroup[index].image}
+                  />
+                );
+              }
+              return null;
+            }
+            return null;
+          })}
         </div>
       </div>
     );
   }
 }
 export default connect(({ global }: ConnectState) => ({
-  solutionGroup: global.solutionGroup, solutionRouter: global.solutionRouter,
+  solutionGroup: global.solutionGroup,
+  solutionRouter: global.solutionRouter,
 }))(favorites);
