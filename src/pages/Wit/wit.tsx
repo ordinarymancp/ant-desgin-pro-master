cc8521d87ff96a627885d94a353c6faaeb68d3d1
cyqyoyoyo <949095688@qq.com>
2019-10-11 16:27:55 +0800

    add new ppt page    2019/10/11

--- src/pages/Wit/wit.tsx
+++ src/pages/Wit/wit.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { connect } from 'dva';
 import s from './index.scss'
 import imgs from '@/assets/66.jpg'
-import example1 from '@/assets/example1.png'
-import example3 from '@/assets/example3.png'
-import example4 from '@/assets/example4.png'
+import example1 from '@/assets/wisdomEducation/example1.jpg'
+import example3 from '@/assets/wisdomEducation/example3.jpg'
+import example4 from '@/assets/wisdomEducation/example4.jpg'
 
 // import Zmage from 'react-zmage'
 import {
@@ -30,14 +30,18 @@ class Addpeople extends React.Component {
     state = {
       scrollTop: 0,
       timer: true,
-      tabGroupA: ['教育云-支持服务运营中心', '教育公共服务管理平台', '教育云资源平台', '人人通网络学习空间平台', '云课堂教学服务平台', '校园综合管理平台'],
+      tabGroupA: [],
       GroupAChecked: 0,
-      tabGroupB: ['张家口教育云一期', '泾川县教育公共服务平台', '基础教育资源公共平台'],
+      tabGroupB: [],
       GroupBChecked: 0,
       hiddenState: true,
       content: '',
       activeIndex: 1,
       coverHidden: true,
+      imagePath: '',
+      info: '',
+      managerA: '',
+      managerB: '',
     };
 
     componentDidMount() {
@@ -51,6 +55,21 @@ class Addpeople extends React.Component {
           this.goNext()
         }
       }
+      const { solutionGroup } = JSON.parse(localStorage.getItem('solutionGroup'));
+      solutionGroup.forEach(item => {
+        item.solutionSonGroup.forEach(items => {
+          if (items.name === match.params.name) {
+            this.setState({
+              tabGroupA: items.tabGroupA,
+              tabGroupB: items.tabGroupB,
+              imagePath: items.imagePath,
+              info: items.info,
+              managerB: items.managerB || '',
+              managerA: items.managerA || '',
+            });
+          }
+        });
+      });
     }
 
     bindHandleScroll = (event)=>{
@@ -186,11 +205,11 @@ class Addpeople extends React.Component {
   };
 
     render() {
-      const { tabGroupA, GroupAChecked, tabGroupB, GroupBChecked, hiddenState, activeIndex, coverHidden } = this.state;
+      const { tabGroupA, GroupAChecked, tabGroupB, GroupBChecked, hiddenState, activeIndex, coverHidden, imagePath, info, managerB, managerA } = this.state;
         return (
             <div style={{width: '100%', height: '100%', background: 'white'}}>
               <div style={{position: 'fixed', height: '100%', width: '100%',background: 'rgba(0,0,0,0.2)', zIndex: '999'}} hidden={coverHidden} onClick={() => {this.setState({coverHidden: true})}}>
-                <img src={require(`../../../src/assets/ppt${activeIndex}.jpg`)} style={{ position: 'absolute', margin: 'auto', left: '0', right: '0', top: '0', bottom: '0', width: '920px', height: '690px'}}/>
+                <img src={imagePath !== '' ? require(`../../../src/assets/${imagePath}/ppt${activeIndex}.jpg`) : ''} style={{ position: 'absolute', margin: 'auto', left: '0', right: '0', top: '0', bottom: '0', width: '920px', height: '690px'}}/>
               </div>
               <div
                 style={{ position: 'fixed', height: '100%', width: '3%', right: 0, zIndex: '999' }}
@@ -236,38 +255,35 @@ class Addpeople extends React.Component {
               </div>
                 <header style={{ position: 'relative' }}>
                   <img src="https://img.alicdn.com/tfs/TB1RR6rz6TpK1RjSZKPXXa3UpXa-1920-648.jpg" alt="" style={{ width: '100%', height: 500, zIndex: '-1', transform: `matrix(1, 0, 0, 1, 0, ${this.state.scrollTop * 0.6})` }} />
-                  <div className={s.titleContent}>智慧教育解决方案</div>
+                  <div className={s.titleContent}>{this.props.match.params.name}解决方案</div>
                   <div className={s.bt}>
                         <span>咨询我们</span>
                     </div>
                 </header>
                 <nav style={ {position: 'relative', background: 'white', zIndex: '3', width: '100%'}}>
                     <ul className={s.nav}>
-                        <li><a href="#overviews">概述</a></li>
-                        <li><a href="#product">总体架构</a></li>
-                        <li><a href="#introduction">方案介绍</a></li>
-                        <li><a href="#manage">运营管理</a></li>
-                        <li><a href="#example">应用案例</a></li>
+                        <li><a href="#overviews" style={{fontWeight: 800}}>概述</a></li>
+                        <li><a href="#product" style={{fontWeight: 800}}>总体架构</a></li>
+                        <li><a href="#introduction" style={{fontWeight: 800}}>方案介绍</a></li>
+                        <li><a href="#manage" style={{fontWeight: 800}}>运营管理</a></li>
+                        <li><a href="#example" style={{fontWeight: 800}}>应用案例</a></li>
                     </ul>
                 </nav>
               {this.state.scrollTop > 500 ? <nav style={{position: 'fixed', background: 'white', top: '0px', zIndex: 99,width: '100%'}}>
                 <ul className={s.nav}>
-                  <li><a href="#overviews">概述</a></li>
-                  <li><a href="#product">总体架构</a></li>
-                  <li><a href="#introduction">方案介绍</a></li>
-                  <li><a href="#manage">运营管理</a></li>
-                  <li><a href="#example">应用案例</a></li>
+                  <li><a href="#overviews" style={{fontWeight: 800}}>概述</a></li>
+                  <li><a href="#product" style={{fontWeight: 800}}>总体架构</a></li>
+                  <li><a href="#introduction" style={{fontWeight: 800}}>方案介绍</a></li>
+                  <li><a href="#manage" style={{fontWeight: 800}}>运营管理</a></li>
+                  <li><a href="#example" style={{fontWeight: 800}}>应用案例</a></li>
                 </ul>
               </nav>: ''}
                 <div id="overviews" className={s.content} style={{ zIndex: '3', position: 'relative', background: 'white'}}>
                     {/* 教育建设 */}
                     <div className={s.wit}>
-                        <h1 className={s.tit}>智慧教育建设内容</h1>
+                        <h1 className={s.tit}>{this.props.match.params.name}建设内容</h1>
                         <div className={s.witcontent}>
-                          智慧教育采用教育云的建设模式，包括“云”和“端”两部分。其中“云”主要包括1个云核心基础支撑平台、1个
-                          支持运营服务中心、5个应用平台（分别为“人人通”平台、教育云资源平台、教育公共服务管理平台、云课堂教学服
-                          务平台、校园综合管理服务平台）；“端”的建设主要是智慧学校应用环境，包括：视频监控系统、门禁一卡通系统、
-                          信息发布系统、图书馆电子阅读系统、智慧教室、未来空间、多媒体互动教室、云桌面系统、WIFI覆盖等。
+                          {info}
                         </div>
                     </div>
                     {/* 总体架构 */}
@@ -275,7 +291,7 @@ class Addpeople extends React.Component {
                         <h1 className={s.tit}>总体架构</h1>
                         <div className={s.witcontent}>
                             <div className={s.titnav}>
-                              <img src={example1} alt="" style={{width: '95%', height: '700px', margin: '0 auto'}}/>
+                              <img src={imagePath !== '' ? require(`../../../src/assets/${imagePath}/example1.jpg`) : ''} alt="" style={{width: '95%', height: '700px', margin: '0 auto'}}/>
                             </div>
                         </div>
                     </div>
@@ -292,7 +308,7 @@ class Addpeople extends React.Component {
                                 }
                               </div>
                               <div className={s.tabRight}>
-                                <img src={require(`../../../src/assets/ppt${GroupAChecked + 1}.jpg`)} alt="" style={{width: '800px', height: '600px'}} onClick={() => {this.setState({coverHidden: false, activeIndex: GroupAChecked + 1})}}/>
+                                <img src={imagePath !== '' ? require(`../../../src/assets/${imagePath}/ppt${GroupAChecked + 1}.jpg`) : ''} alt="" style={{width: '800px', height: '600px'}} onClick={() => {this.setState({coverHidden: false, activeIndex: GroupAChecked + 1})}}/>
                               </div>
                             </div>
                         </div>
@@ -302,20 +318,17 @@ class Addpeople extends React.Component {
                         <h1 className={s.tit}>运营管理</h1>
                         <div className={s.witcontent}>
                             <div style={{ paddingLeft: 15, paddingRight: 15, }}>
-                                <p style={{ marginTop: 20, fontSize: 18 }}>在建设好智慧教育系统的同时，通过高效的运营，既充分发挥出了智慧教育系统的优势，又为社会创造了经济效益和社会效益。
-                                  通过教育云运营，切实发挥智慧教育系统的作用，扩大优质资源覆盖面，缩小城乡、重点学校和非重点学校之间的差别，加强教
-                                  育管理，全面提高教育水平，实现教育部提出的教育信息化各项核心目标。</p>
-                                <p style={{ marginTop: 15, fontSize: 18, fontWeight: 700 }}>智慧教育的运营分两种模式，基础服务运营和增值服务运营。这两种服务均由运营公司管理运营，运营公司的建立有多种模式，
-                                  比如政府委托第三方，或由SPV公司建立。</p>
+                                <p style={{ marginTop: 20, fontSize: 18 }}>{managerA}</p>
+                                <p style={{ marginTop: 15, fontSize: 18, fontWeight: 700 }}>{managerB}</p>
                                 <div className={s.tab}>
                                     <div className={s.tabson}>
                                       <div className={s.tabCon}>
-                                        <img src={example3} alt="" style={{width: '100%', height: '100%'}}/>
+                                        <img src={imagePath !== '' ? require(`../../../src/assets/${imagePath}/example3.jpg`) : ''} alt="" style={{width: '100%', height: '100%'}}/>
                                       </div>
                                     </div>
                                   <div className={s.tabson}>
                                     <div className={s.tabCon}>
-                                      <img src={example4} alt="" style={{width: '100%', height: '100%'}}/>
+                                      <img src={imagePath !== '' ? require(`../../../src/assets/${imagePath}/example4.jpg`) : ''} alt="" style={{width: '100%', height: '100%'}}/>
                                     </div>
                                   </div>
                                 </div>
@@ -335,7 +348,7 @@ class Addpeople extends React.Component {
                             }
                           </div>
                           <div className={s.tabRight}>
-                            <img src={require(`../../../src/assets/ppt${GroupBChecked + 7}.jpg`)} alt="" style={{width: '800px', height: '600px'}} onClick={() => {this.setState({coverHidden: false, activeIndex: GroupBChecked + 7})}}/>
+                            <img src={imagePath !== '' ? require(`../../../src/assets/${imagePath}/ppt${GroupBChecked + tabGroupA.length + 1 }.jpg`) : ''} alt="" style={{width: '800px', height: '600px'}} onClick={() => {this.setState({coverHidden: false, activeIndex: GroupBChecked + tabGroupA.length + 1})}}/>
                           </div>
                         </div>
                       </div>
